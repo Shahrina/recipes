6ffce5791480bf5a91011169f7e59d70695d12a4
Shahrina Uddin <shahrina94@hotmail.com>
2025-05-27 22:18:36 +0200

    Use Coil for images.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.moshi.kotlin)
     implementation(libs.moshiConverter)
     implementation(libs.navigation.compose)
+    implementation(libs.coil.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
