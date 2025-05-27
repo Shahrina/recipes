d38b9b086ea4b7525c40191ba0215b9cf90469b8
Shahrina Uddin <shahrina94@hotmail.com>
2025-05-27 22:18:59 +0200

    Update recipe data class.

--- app/src/main/java/com/example/recipes/data/model/Recipe.kt
+++ app/src/main/java/com/example/recipes/data/model/Recipe.kt
@@ -1,7 +1,14 @@
 package com.example.recipes.data.model
 
 data class Recipe(
+    val id: Int,
     val title: String,
+    val image: String?,
     val course: String,
     val cuisine: String,
+    val ingredients: List<String> = emptyList(),
+    val instructions: String?,
+    val servings: Int?,
+    val prepTime: String?,
+    val cookTime: String?
 )
